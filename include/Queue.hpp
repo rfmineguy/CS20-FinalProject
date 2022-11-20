220bc0f7ca03ad2d04f3208ac0d8fad2e300c634
rfmineguy <rfmineguy@gmail.com>
2022-11-20 13:54:49 -0800

    Finished base header for Queue

--- include/Queue.hpp
+++ include/Queue.hpp
@@ -1,4 +1,14 @@
 #ifndef QUEUE_HPP
 #define QUEUE_HPP
 
+template <typename T>
+class Queue {
+public:
+  Queue();
+  virtual ~Queue();
+  virtual void Enqueue(const T& elem) = 0;
+  virtual T Dequeue() = 0;
+  virtual int Size() = 0;
+};
+
 #endif
